0faad65e2d51785b5adb3784b94c05cbe7907b94
Sam Sauer <sauer@ffmmbp-051.hom-de.syzygy.intern>
2018-05-23 17:27:03 +0200

    refactor

--- src/states/Main.js
+++ src/states/Main.js
@@ -60,7 +60,7 @@ export default class Main extends Phaser.State {
 		// update frames
 		console.log('update;');
 
-		var hitPlatform = this.game.physics.arcade.collide(this.player, this.platforms);
+		const hitPlatform = this.game.physics.arcade.collide(this.player, this.platforms);
 
 		this.cursors = this.game.input.keyboard.createCursorKeys();
 
@@ -71,28 +71,25 @@ export default class Main extends Phaser.State {
 	    {
 	        //  Move to the left
 	        this.player.body.velocity.x = -150;
-
 	        this.player.animations.play('left');
 	    }
 	    else if (this.cursors.right.isDown)
 	    {
 	        //  Move to the right
 	        this.player.body.velocity.x = 150;
-
 	        this.player.animations.play('right');
 	    }
 	    else
 	    {
 	        //  Stand still
 	        this.player.animations.stop();
-
 	        this.player.frame = 4;
 	    }
 
 	    //  Allow the player to jump if they are touching the ground.
 	    if (this.cursors.up.isDown && this.player.body.touching.down && hitPlatform)
 	    {
-	        this.player.body.velocity.y = -350;
+	        this.player.body.velocity.y = -150;
 	    }
 	}
 }
