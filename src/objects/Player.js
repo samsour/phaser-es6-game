9c2b481f2d0beb2874a478fca32cff8c972b9e5d
Sam Sauer <sauer@ffmmbp-051.hom-de.syzygy.intern>
2018-05-25 12:24:48 +0200

    player hitbox and bullet rotation

--- src/objects/Player.js
+++ src/objects/Player.js
@@ -37,6 +37,8 @@ export default class Player {
 		this.sprite.smoothed = false;
 		this.sprite.scale.set(5,5);
 
+		this.sprite.body.setSize(10,14,0,0);
+
 	    //  Some animations, walking left and right.
 	    this.sprite.animations.add('right', [1, 2], 5, true);
 		this.sprite.animations.add('left', [4,5], 5, true);
@@ -44,13 +46,18 @@ export default class Player {
 		this.sprite.animations.add('idle-left', [3,18], 1.5, true);
 
 		// Add a weapon to the player
-		this.weapon = this.game.add.weapon(30, 'archer');
+		this.weapon = this.game.add.weapon(30, 'arrow');
 		this.weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
 		//  The speed at which the bullet is fired
 		this.weapon.bulletSpeed = this.bulletSpeed;
 		//  Speed-up the rate of fire, allowing them to shoot 1 bullet every 60ms
 		this.weapon.fireRate = this.attackSpeed;
 		this.weapon.trackSprite(this.sprite, 0, 0, true);
+
+		this.weapon.bullets.forEach((b) => {
+			b.scale.setTo(5, 5);
+			b.angle = 90;
+		}, this);
 	}
 
 	moveLeft() {
@@ -82,9 +89,20 @@ export default class Player {
 	fire() {
 		if(this.direction == "left"){
 			this.weapon.bulletSpeed = -(this.bulletSpeed);
+			this.weapon.bullets.forEach((b) => {
+				b.angle = -90;
+				b.body.updateBounds();
+			}, this);
 		} else {
 			this.weapon.bulletSpeed = this.bulletSpeed;
+			this.weapon.bullets.forEach((b) => {
+				b.angle = 90;
+				b.body.updateBounds();
+			}, this);
 		}
+
+		
+
 		this.weapon.fire();
 	}
 
